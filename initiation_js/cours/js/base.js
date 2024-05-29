ce0fac62d9f8e6b3c7187ad1edadceb7c4539620
Maxime <maxime.loir@lepoles.org>
2024-05-29 16:53:16 +0200

    cours java script 29/05/2024

---
+++ initiation_js/cours/js/base.js
@@ -0,0 +1,35 @@
+var prenom,
+  monNom = "Loir";
+alert(monNom);
+var prenomPourMonProjet;
+
+// - - 2 : Affecter une valeur à une variable
+prenom = "Maxime";
+
+alert(prenom);
+
+console.log(prenom);
+
+document.write(monNom + "<br>");
+document.write(prenom + "<br>");
+document.write("RADWIMPS !" + "<br>");
+
+var jourSemaine = window.prompt(
+  "Question : on est quel jour?",
+  "jour de la semaine" + "<br>"
+);
+
+document.write(jourSemaine);
+console.log("jour de la semaine");
+console.log("Mercredi");
+console.log(jourSemaine);
+
+var variable = "ma variable";
+var unChiffre = 2;
+var cetteAnnee = 2024;
+var stringAddition = "2+5";
+var addition2Number = 2 + 6;
+
+alert(variable);
+document.write(unChiffre);
+console.log(cetteAnnee, stringAddition, addition2Number);
