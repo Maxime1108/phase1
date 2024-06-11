2690416cf9e3c9434c2fa43202159d8d997ad4af
Maxime <maxime.loir@lepoles.org>
2024-06-11 16:51:43 +0200

    11/06/24

---
+++ initiation_js/cours/js/baccalaureat.js
@@ -0,0 +1,29 @@
+// // Demander la moyenne du candidat
+// let moyenne = prompt("Veuillez entrer la moyenne du candidat :");
+
+// // Convertir la moyenne en nombre
+// moyenne = parseFloat(moyenne);
+
+// // Vérifier et afficher le résultat en fonction de la moyenne
+// if (moyenne < 10) {
+//   alert("Recalé");
+// } else if (moyenne >= 10 && moyenne < 12) {
+//   alert("Reçu");
+// } else if (moyenne >= 12) {
+//   alert("Reçu avec mention");
+// } else {
+//   alert("Valeur non valide");
+// }
+
+let valeur = prompt("Veuillez entrer une valeur : ");
+parseInt(valeur);
+console.log(isNaN(valeur));
+
+// // Vérifier le type de la valeur saisie
+// if (typeof userInput === "string") {
+//   // Afficher la valeur saisie
+//   alert("La valeur saisie est : " + userInput);
+// } else {
+//   // Afficher un message d'erreur
+//   alert("Le type de votre valeur n'est pas un 'string' !");
+// }
